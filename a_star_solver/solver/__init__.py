ea608e58c7ffd2cd9227e6fb366ba5239e3adbfe
billos23 <billblade14@gmail.com>
2025-12-01 18:10:09 +0200

    Update __init__.py

--- a_star_solver/solver/__init__.py
+++ a_star_solver/solver/__init__.py
@@ -1,9 +1,1 @@
-"""
-Solver module containing the core A* algorithm implementation.
-"""
 
-from .a_star import AStarSolver
-from .sokoban_heuristics import SokobanManhattanHeuristic, SokobanSimpleHeuristic
-from .solution_result import SolutionResult
-
-__all__ = ['AStarSolver', 'SokobanManhattanHeuristic', 'SokobanSimpleHeuristic', 'SolutionResult']

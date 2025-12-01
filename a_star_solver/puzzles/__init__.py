f027ed0cb34a08b537b837aca3c2b57d4e29066d
billos23 <billblade14@gmail.com>
2025-12-01 18:10:25 +0200

    Update __init__.py

--- a_star_solver/puzzles/__init__.py
+++ a_star_solver/puzzles/__init__.py
@@ -1,9 +1,1 @@
-"""
-Puzzles module containing puzzle state representations and implementations.
-"""
 
-from .base_puzzle import PuzzleState
-from .sokoban_puzzle import SokobanState
-from .sokoban_loader import SokobanLevelLoader, SokobanLevel
-
-__all__ = ['PuzzleState', 'SokobanState', 'SokobanLevelLoader', 'SokobanLevel']

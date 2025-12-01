bd37071cabd528249713d21d92fd93a1bf9ce714
billos23 <billblade14@gmail.com>
2025-12-01 18:13:45 +0200

    Update main.py

--- main.py
+++ main.py
@@ -125,7 +125,7 @@ if __name__ == "__main__":
         sys.exit(0)
     except Exception as e:
         print(f"\nError: {e}")
-        import traceback
-        traceback.print_exc()
+    
 
         sys.exit(1)
+
